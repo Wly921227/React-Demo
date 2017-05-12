856f32933394468f4aea7ae6822e6408e82919e1
wangluyuan <wangluyuan@yeecall.com>
2017-05-12 10:59:10 +0800

    props.children 添加属性

--- src/pages/component/children/index.js
+++ src/pages/component/children/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-class Child1 extends React.Component {
+class Child extends React.Component {
     render() {
         return (<div className="child-1">
             Child one's name is {this.props.childName}.
+            <div style={{color: 'red'}}>{this.props.fromParent}</div>
         </div>)
     }
 }
@@ -13,10 +14,18 @@ class Child1 extends React.Component {
  */
 class HasChildren extends React.Component {
     render() {
+        let children = React.Children.map(this.props.children, child => {
+            if (child.type === Child) {
+                return React.cloneElement(child, {
+                    fromParent: 'from parent props'
+                })
+            }
+        })
+
         return (<div className="has-children">
             这里是包含子节点的组件：{this.props.title}
             <div className="children">
-                {this.props.children}
+                {children}
             </div>
         </div>)
     }
@@ -26,8 +35,11 @@ class Parent extends React.Component {
     render() {
         return (<div className="parent">
             <HasChildren title="我有两个子节点">
-                <Child1 childName="子节点1"/>
-                <div>子节点2</div>
+                <Child childName="子节点1"/>
+                <Child childName="子节点2"/>
+                <Child childName="子节点3"/>
+                <div>子节点其他</div>
+                <Child childName="子节点4"/>
             </HasChildren>
         </div>)
     }
